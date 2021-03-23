8bb4c394b2eb54d605a7c6ce6ed86332e5dd44e0
Anze Slosar <anze@bnl.gov>
2021-03-23 12:52:36 -0400

    /packages install, 2021.03

--- settings.sh
+++ settings.sh
@@ -1,4 +1,4 @@
-TARGET=/gpfs02/astro/workarea/anze/bacon
+TARGET=/gpfs02/astro/packages/bacon/2021.03/
 TMP=/tmp/
 ANACONDA=https://repo.anaconda.com/archive/Anaconda3-2020.11-Linux-x86_64.sh
 CONDA_PACKAGES="healpy pyccl astropy fitsio"
