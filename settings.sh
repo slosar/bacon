3e6eebb095a80b65f2f2ada4f22042fee6d8bebb
Anze Slosar <anze@bnl.gov>
2021-11-23 09:09:09 -0500

    21.09 version

--- settings.sh
+++ settings.sh
@@ -1,5 +1,8 @@
-TARGET=/gpfs02/astro/packages/bacon/2021.03/
+TARGET=/gpfs02/astro/packages/bacon/2021.09/
 TMP=/tmp/
-ANACONDA=https://repo.anaconda.com/archive/Anaconda3-2020.11-Linux-x86_64.sh
-CONDA_PACKAGES="healpy pyccl astropy fitsio galsim numba ngmix esutil lsstdesc.weaklensingdeblending esutil spiceypy"
-REPOS="bmxdemo/bmxobs bmxdemo/bmxdaq LSSTDESC/descwl-shear-sims telegraphic/pygdsm "
+ANACONDA=https://repo.anaconda.com/archive/Anaconda3-2021.05-Linux-x86_64.sh
+CONDA_PACKAGES="pyccl astropy fitsio healpy spiceypy pyfftw galsim numba ngmix esutil"
+# don't add pyside2
+REPOS_NODEPS="bmxdemo/bmxobs bmxdemo/bmxdaq LSSTDESC/descwl-shear-sims"
+REPOS="telegraphic/pygdsm"
+
--- setup_conda.sh
+++ setup_conda.sh
@@ -4,17 +4,9 @@ source settings.sh
 CONDA=$TARGET/bin/conda
 PIP=$TARGET/bin/pip
 
-
-## install conda packages
+# install conda packages
 $CONDA install -c conda-forge -y $CONDA_PACKAGES
 if [ $? -ne 0 ] ; then
    echo "Failed to install " $CONDA_PACKAGES
    exit
 fi
-
-for repo in $REPOS
-{
-  $PIP install --no-deps git+https://github.com/$repo
-}
-
-
