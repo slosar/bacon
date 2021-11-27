06d98dfb47f6e1d8e8a219dbe799ef403a9d34ce
Anze Slosar <anze@bnl.gov>
2021-11-27 15:02:43 -0500

    dask and cvmfs

--- settings.sh
+++ settings.sh
@@ -1,7 +1,7 @@
-TARGET=/gpfs02/astro/packages/bacon/2021.09/
+TARGET=/cvmfs/astro.sdcc.bnl.gov/SL73/packages/bacon/2021.09/
 TMP=/tmp/
 ANACONDA=https://repo.anaconda.com/archive/Anaconda3-2021.05-Linux-x86_64.sh
-CONDA_PACKAGES="pyccl astropy fitsio healpy spiceypy pyfftw galsim numba ngmix esutil"
+CONDA_PACKAGES="pyccl astropy fitsio healpy spiceypy pyfftw galsim numba ngmix esutil dask"
 # don't add pyside2
 REPOS_NODEPS="bmxdemo/bmxobs bmxdemo/bmxdaq LSSTDESC/descwl-shear-sims"
 REPOS="telegraphic/pygdsm"
